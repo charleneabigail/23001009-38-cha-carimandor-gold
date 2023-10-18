4c9d1b665256f0ce6852c99340dee829f6d93870
Abel <charleneabigail97@gmail.com>
2023-10-18 17:35:30 +0700

    user controllers-routes

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 const routes = require('./routes')
+const datas = require('./db/data.json')
+const fs = require ('fs')
+//const datas2 = fs.readFileSync('./db/data.json', utf-8)
 
 app.use(express.json()) 
 app.use(express.urlencoded({extended: true}))
 app.use('/static', express.static('public'))
 
 app.set('view engine', 'ejs')
+
+//GET ALL DATA
+app.get('/api/post', (req, res) => {
+    res.status(200).json(datas)
+})
 
 
 // application level middleware
@@ -23,7 +31,7 @@ app.use('/', routes)
 
 // middleware error handler
 const errorMidware = (err, req, res, next) => {
-    console.log('ini error');
+    console.log(err);
 
     res.status(500).send({
         error: {
